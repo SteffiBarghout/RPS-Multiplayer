6f1cad656b6f3c3b298f313e06ca7bf5c45bf4ef
Steffi Wellington <steffisbootcampdrive@gmail.com>
2020-03-18 22:54:57 -0700

    variables displaying numbers of wins, losses, ties

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+//Array of Computer's possible choices.
+let computerChoices = ["r", "p", "s"];
+
+//Create variables to hold the numbers of wins, losses, ties.
+let wins = 0;
+let losses = 0;
+let ties = 0;
